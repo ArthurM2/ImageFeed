fc2b6a808eac4ae8f57fa205a33389b41af095b7
ArthurM2 <arthur.komarov@gmail.com>
2023-09-08 01:04:01 +0500

    добавил аутлеты

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usertagLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    
+}
