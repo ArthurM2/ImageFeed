30f17b9dde87d804b972cb9644365d593e2d029d
ArthurM2 <arthur.komarov@gmail.com>
2023-09-09 12:07:44 +0500

    - импортировал иконки - добавил таббар

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -2,12 +2,21 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    // MARK: - Outlets
     @IBOutlet weak var avatarImageView: UIImageView!
-    
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var usertagLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
     @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func tapLogoutButton(_ sender: Any) {
+        print("✅button pressed🍏")
+    }
+    
+    // MARK: - StatusBar style
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent;
+    }
     
 }
